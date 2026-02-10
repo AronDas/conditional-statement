6211a97957b2cb3f58c778d9163d866e9e79afd8
AronDas <arondas.sg@gmail.com>
2026-02-10 18:47:17 -0400

    Add files via upload

---
+++ greaterthanorsmallerthan.py
@@ -0,0 +1,8 @@
+a = int(input("Please enter a number here:"))
+if (a > 15):
+    print("This number is greater than 15")
+    print("Im an if block")
+else:
+    print("This number is less than 15")
+    print("Im an else block")
+print("Im not an if block or an else block")
