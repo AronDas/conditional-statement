fae3457853783d9ca7919d4e31025986e61596d1
AronDas <arondas.sg@gmail.com>
2026-02-11 18:54:01 -0400

    Add files via upload

---
+++ notequaloperator.py
@@ -0,0 +1,34 @@
+a = 1
+
+b = 2
+
+c = 3
+
+print (a != b)
+print (b != c)
+
+
+d = "Python"
+
+e = "Coding"
+
+if d != e:
+    print (d ,"and", e ,"are not the same")
+
+
+f = 4
+
+g = 5
+
+if (f == 4 and g == 5):
+    print("Hello")
+
+
+
+
+h = int(input("Please enter a number"))
+
+if h%2 != 0:
+    print (h ,"Is not an even number")
+else:
+    print (h ,"Is an even number")
