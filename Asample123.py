de56eb7bded73783682196cd77077f95e1b50e0a
AronDas <arondas.sg@gmail.com>
2026-02-11 18:42:26 -0400

    Add files via upload

---
+++ Asample123.py
@@ -0,0 +1,21 @@
+
+a = 10
+
+b = 10
+
+c = False
+
+if a > 0 and b > 0:
+    print ("Both numbers are above zero.")
+print (not c)
+
+d = 10
+
+e = -14
+
+f = 0
+
+if d and e and f:
+    print ("All numbers have boolean value as true")
+else:
+    print ("Atleast one number has a boolean value as false")
