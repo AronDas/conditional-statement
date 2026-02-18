ab68326eca18a4b0fe36421e4c68cfe7e2316920
AronDas <arondas.sg@gmail.com>
2026-02-18 18:57:00 -0400

    Add files via upload

---
+++ correctmean.py
@@ -0,0 +1,13 @@
+mean1 = 28
+wrongnumber = 26
+correctnumber = 46
+totalnumber = 30
+
+sum = mean1*totalnumber
+print("the sum of 30 number is:", sum)
+
+num2 = sum-((wrongnumber)-(correctnumber))
+print("sum - wrongnumber - correctnumber", num2)
+
+mean2 = num2/totalnumber
+print(mean2)
