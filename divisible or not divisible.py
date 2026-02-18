aeb7d26798555283ec8248c2c33dd40f3089f539
AronDas <arondas.sg@gmail.com>
2026-02-18 18:45:53 -0400

    Add files via upload

---
+++ divisible or not divisible.py
@@ -0,0 +1,7 @@
+a1 = int(input("please enter your numerator here:"))
+a2 = int(input("Please enter your denominator here:"))
+
+if a1%a2 == 0:
+    print("This number is divisble by 2")
+else:
+    print("This number is not divislbe by 2")
