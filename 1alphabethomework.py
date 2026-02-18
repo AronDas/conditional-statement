fd2dcb9ba7123199b699c9fcc8c8bf1075b5656f
AronDas <arondas.sg@gmail.com>
2026-02-18 19:08:25 -0400

    Add files via upload

---
+++ 1alphabethomework.py
@@ -0,0 +1,8 @@
+a1 = (input("Please enter the letter here:"))
+
+a2 = ord(a1)
+
+if a2 >= 65 and a2 <= 122: 
+    print("This letter is in the alphabet")
+else:
+    print("This letter is not in the alphabet.")
