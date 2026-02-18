912e238ac90d5e2201b8f9b5b562ceaea9b0cd5f
AronDas <arondas.sg@gmail.com>
2026-02-18 18:38:48 -0400

    Add files via upload

---
+++ operatorspresidence.py
@@ -0,0 +1,15 @@
+a = 3
+b = 9
+c = 1
+d = 7
+e = 0
+e = (a + d)**(c+b)%d 
+print("the value of a+d**c+d divided by b is:", e)
+
+
+name = "Aron"
+age = 12
+if name == "Aron" or name == "Alex" and age >= 2:
+    print("hello!, welcome")
+else:
+    print("goodbye!")
