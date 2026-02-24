8840a8a0e8ce2db0f9c44de581b659770a4e741f
AronDas <arondas.sg@gmail.com>
2026-02-24 18:59:09 -0400

    Add files via upload

---
+++ agechecker.py
@@ -0,0 +1,8 @@
+age = int(input("Please enter your age here:"))
+
+if age >= 20:
+    print("Sory, you are unable to enroll in this class.")
+elif age <= 9:
+    print("Sorry, you are unable to enroll in this class.")
+else:   
+    print("You are able to enroll in this class!")
